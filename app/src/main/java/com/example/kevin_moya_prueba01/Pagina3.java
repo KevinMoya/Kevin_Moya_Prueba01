221dd6d4bd582f923488093671a1690a71a1b2a6
ESTUDIANTES <kmoya77883uta.edu.ec>
2022-12-01 17:29:49 -0500

    commit 17:30

---
+++ app/src/main/java/com/example/kevin_moya_prueba01/Pagina3.java
@@ -0,0 +1,14 @@
+package com.example.kevin_moya_prueba01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Pagina3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pagina3);
+    }
+}
